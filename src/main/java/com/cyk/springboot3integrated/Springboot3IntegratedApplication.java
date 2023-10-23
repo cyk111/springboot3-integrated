edf5e0d0f11383e9c4febe5fff1f09a415ed16f4
cyk <chenyongke111@163.com>
2023-10-23 21:43:44 +0800

    Initial commit

---
+++ src/main/java/com/cyk/springboot3integrated/Springboot3IntegratedApplication.java
@@ -0,0 +1,13 @@
+package com.cyk.springboot3integrated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3IntegratedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3IntegratedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyk/springboot3integrated/Springboot3IntegratedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyk.springboot3integrated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3IntegratedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
