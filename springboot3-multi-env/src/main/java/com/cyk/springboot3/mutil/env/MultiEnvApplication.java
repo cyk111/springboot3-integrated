a154256edde48dfa9aa1c777d2e660a262859e53
cyk <chenyongke111@163.com>
2023-10-29 08:19:47 +0800

    多环境切换  mvn clean install -P***

---
+++ springboot3-multi-env/src/main/java/com/cyk/springboot3/mutil/env/MultiEnvApplication.java
@@ -0,0 +1,16 @@
+package com.cyk.springboot3.mutil.env;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cyk
+ * @date 2023/10/29 08:06
+ */
+@SpringBootApplication
+public class MultiEnvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiEnvApplication.class,args);
+    }
+}
