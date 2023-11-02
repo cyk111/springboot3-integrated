008c1dedb2330ba24ea869c2f4073f8318900399
cyk <chenyongke111@163.com>
2023-11-02 21:49:23 +0800

    springboot 集成dockercompose

---
+++ springboot3-docker-compose/src/main/java/com/cym/springboot3/dockercompose/DockerComposeApplication.java
@@ -0,0 +1,13 @@
+package com.cym.springboot3.dockercompose;
+
+/**
+ * @author cyk
+ * @date 2023/11/2 21:45
+ */
+@SpringBootApplication
+public class DockerComposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerComposeApplication.class,args);
+    }
+}
