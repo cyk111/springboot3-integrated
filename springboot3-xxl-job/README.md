0db2b08bde520f08f33f341a24dc7d1f978208aa
cyk <chenyongke111@163.com>
2023-11-06 17:51:30 +0800

    springboot 集成quartz

---
+++ springboot3-xxl-job/README.md
@@ -0,0 +1,1 @@
+![img.png](xxl_job_img.png)
---
+++ springboot3-xxl-job/src/main/java/com/cyk/springboot3/xxl/job/XxlJobApplication.java
@@ -0,0 +1,17 @@
+package com.cyk.springboot3.xxl.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cyk
+ * @date 2023/11/6 16:56
+ */
+@SpringBootApplication
+public class XxlJobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobApplication.class,args);
+    }
+
+
+}
