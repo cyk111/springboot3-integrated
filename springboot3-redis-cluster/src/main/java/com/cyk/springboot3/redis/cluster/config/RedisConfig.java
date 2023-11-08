f56dab95e82feca5b6ece2fa2f9892159582d96d
cyk <chenyongke111@163.com>
2023-11-08 19:54:42 +0800

    springboot 集群模式增加读写分离bean

--- springboot3-redis-cluster/src/main/java/com/cyk/springboot3/redis/cluster/config/RedisConfig.java
+++ springboot3-redis-cluster/src/main/java/com/cyk/springboot3/redis/cluster/config/RedisConfig.java
@@ -1,12 +1,15 @@
 package com.cyk.springboot3.redis.cluster.config;
 
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import io.lettuce.core.ReadFrom;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -26,7 +29,7 @@ public class RedisConfig {
     @Value("${spring.data.redis.cluster.nodes}")
     private List<String> nodes;
 
-    @Bean
+    @Bean("redisConnectionFactory")
     public LettuceConnectionFactory lettuceConnectionFactory() {
         // 集群配置
         RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
@@ -38,8 +41,23 @@ public class RedisConfig {
         return new LettuceConnectionFactory(clusterConfiguration);
     }
 
+    @Bean("redisConnectionFactoryReadOnly")
+    public LettuceConnectionFactory redisConnectionFactory() {
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .readFrom(ReadFrom.SLAVE_PREFERRED)
+                .build();
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
+        for (String node : nodes) {
+            String[] arr = node.split(":");
+            clusterConfiguration.addClusterNode(new RedisNode(arr[0], Integer.parseInt(arr[1])));
+        }
+        clusterConfiguration.setPassword(RedisPassword.of(password));
+
+        return new LettuceConnectionFactory(clusterConfiguration, clientConfig);
+    }
+
     @Bean
-    public RedisTemplate<String,Object> redisTemplate(LettuceConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String,Object> redisTemplate(@Qualifier("redisConnectionFactory") LettuceConnectionFactory redisConnectionFactory) {
         RedisTemplate<String,Object> template = new RedisTemplate<>();
         //使用fastjson序列化
         FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);
--- springboot3-redis-sentinel/src/main/java/com/cyk/springboot3/redis/sentinel/SentinelRedisApplication.java
+++ springboot3-redis-sentinel/src/main/java/com/cyk/springboot3/redis/sentinel/SentinelRedisApplication.java
@@ -14,4 +14,27 @@ public class SentinelRedisApplication {
         SpringApplication.run(SentinelRedisApplication.class,args);
     }
 
+    /***
+     *
+     * 主从模式:
+     * - 优点：
+     *   master能自动将数据同步到slave，可以进行读写分离，分担master的读压力
+     *   master、slave之间的同步是以非阻塞的方式进行的，同步期间，客户端仍然可以提交查询或更新请求
+     * - 缺点：
+     *   不具备自动容错与恢复功能，master或slave的宕机都可能导致客户端请求失败，需要等待机器重启或手动切换客户端IP才能恢复
+     *   master宕机，如果宕机前数据没有同步完，则切换IP后会存在数据不一致的问题
+     *   难以支持在线扩容，Redis的容量受限于单机配置
+     *
+     * 哨兵模式:
+     *  - 有点:
+     *    哨兵模式基于主从复制模式，所以主从复制模式有的优点，哨兵模式也有
+     *    哨兵模式下，master挂掉可以自动进行切换，系统可用性更高
+     *  - 缺点：
+     *    同样也继承了主从模式难以在线扩容的缺点，Redis的容量受限于单机配置
+     *    需要额外的资源来启动sentinel进程，实现相对复杂一点，同时slave节点作为备份节点不提供服务
+
+     *
+     *
+     */
+
 }
